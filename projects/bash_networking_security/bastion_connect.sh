20180dfcb4066a510882b4184aeb18910a8bde3f
Osher Atias <oshernet@gmail.com>
2023-06-11 23:12:20 +0300

    Added bastion script

--- projects/bash_networking_security/bastion_connect.sh
+++ projects/bash_networking_security/bastion_connect.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+PUBLIC_INSTANCE_IP=$1
+PRIVATE_INSTANCE_IP=$2
+COMMAND=$3
+
+
+if [[ -z "${KEY_PATH+x}" ]]; then
+	echo "KEY_PATH env var is expected";
+fi
+
+if [[ -z "${PUBLIC_INSTANCE_IP}" ]]; then
+	echo "Please provide bastion IP address";
+fi
+
+if [ $# -eq 1 ]; then
+  ssh -i "$KEY_PATH" "ubuntu@$PUBLIC_INSTANCE_IP"
+elif [ $# -eq 2 ]; then
+  ssh -i "$KEY_PATH" "ubuntu@$PUBLIC_INSTANCE_IP" ssh -i "~/work/bootcamp.pem" -tt "ubuntu@$PRIVATE_INSTANCE_IP"
+elif [ $# -eq 3 ]; then
+  ssh -i "$KEY_PATH" "ubuntu@$PUBLIC_INSTANCE_IP" ssh -i "~/work/bootcamp.pem" -tt "ubuntu@$PRIVATE_INSTANCE_IP" "$COMMAND"
+else
+  echo "Invalid number of arguments."
+fi
+
+
